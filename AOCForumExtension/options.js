68d7f1351a0d0d0c9eaab6d1bfef38335529484e
awoolstrum <awoolstum@gmail.com>
2017-10-11 13:00:09 -0500

    next verson

--- AOCForumExtension/options.js
+++ AOCForumExtension/options.js
@@ -13,21 +13,23 @@ function save_options() {
 	var Recruitment = document.getElementById('Recruitment').checked;	
 	var Off = document.getElementById('Off').checked;	
 	
-  chrome.storage.sync.set({
-    Theme: Theme,
-    ShowSignatures: ShowSignatures,
-	TheNewlyArrived: TheNewlyArrived,
-	Kickstarter: Kickstarter,
-	Recruitment: Recruitment,
-	Off: Off
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 1500);
-  });  
+	var setOptions = {
+		Theme: Theme,
+		ShowSignatures: ShowSignatures,
+		TheNewlyArrived: TheNewlyArrived,
+		Kickstarter: Kickstarter,
+		Recruitment: Recruitment,
+		Off: Off
+	  };
+	
+  chrome.storage.sync.set( setOptions, function() {
+	// Update status to let user know options were saved.
+	var status = document.getElementById('status');
+	status.textContent = 'Options saved.';
+	setTimeout(function() {
+	  status.textContent = '';
+	}, 1500);
+  });  		
   
 //  document.getElementById('progress').innerHTML = "Saved.";
   
@@ -41,19 +43,19 @@ function restore_options() {
 
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
-    Theme: 'wider',
-    ShowSignatures: true,
-    TheNewlyArrived: false,
-    Kickstarter: false,
-    Recruitment: false,
-	Off: false
-  }, function(items) {
-    document.getElementById('Theme').value = items.Theme;
-    document.getElementById('ShowSignatures').checked = items.ShowSignatures;
-    document.getElementById('TheNewlyArrived').checked = items.TheNewlyArrived;
-    document.getElementById('Kickstarter').checked = items.Kickstarter;
-    document.getElementById('Recruitment').checked = items.Recruitment; 
-	document.getElementById('Off').checked = items.Off; 
+		Theme: 'wider',
+		ShowSignatures: true,
+		TheNewlyArrived: false,
+		Kickstarter: false,
+		Recruitment: false,
+		Off: false
+	  }, function(items) {
+		document.getElementById('Theme').value = items.Theme;
+		document.getElementById('ShowSignatures').checked = items.ShowSignatures;
+		document.getElementById('TheNewlyArrived').checked = items.TheNewlyArrived;
+		document.getElementById('Kickstarter').checked = items.Kickstarter;
+		document.getElementById('Recruitment').checked = items.Recruitment; 
+		document.getElementById('Off').checked = items.Off; 
   });
   
 //  document.getElementById('progress').innerHTML = "Values loaded.";
